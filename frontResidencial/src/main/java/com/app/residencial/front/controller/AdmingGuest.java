3002e6314dae384fe2ff4d8e6a4c2181320f3ddb
Santiago Alzate <santialpo@gmail.com>
2020-06-16 21:21:55 -0500

    Cambios para agregar registro visitates

---
+++ frontResidencial/src/main/java/com/app/residencial/front/controller/AdmingGuest.java
@@ -0,0 +1,20 @@
+package com.app.residencial.front.controller;
+
+import java.util.Date;
+
+import javax.inject.Named;
+
+@Named("guest")
+public class AdmingGuest {
+
+	private Date date2;
+
+	public Date getDate2() {
+		return date2;
+	}
+
+	public void setDate2(Date date2) {
+		this.date2 = date2;
+	}
+
+}
